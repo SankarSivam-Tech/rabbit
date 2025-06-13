a2a5b3b1fa5f8026cc3ced8080666c2a0a5dc85c
Veera Sankar <sankarsivam6991@gmail.com>
2025-06-13 14:26:59 +0530

    complete New Arrival Section UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,5 +10,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+     scrollbarHide
+  ],
 };

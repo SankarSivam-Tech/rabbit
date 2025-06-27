346046b68033edb2132e618faa55a2fe02cb7e55
Veera Sankar <sankarsivam6991@gmail.com>
2025-06-27 14:27:54 +0530

    insert the product data and seed it in database

---
+++ backend/seeder.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("./models/Product");
+const User = require("./models/user");
+const products = require("./data/products");
+
+dotenv.config();
+
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// Function to seed data
+
+const seedData = async () => {
+  try {
+    // clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // create a default admin user
+    const createdUser = await User.create({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
+
+    // Assign default userId to each Product
+    const userID = createdUser._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    // Insert the products into DB
+    await Product.insertMany(sampleProducts);
+
+    console.log("Product data seeded successfully!");
+    process.exit();
+  } catch (error) {
+    console.error("Error seeding the data:", error);
+    process.exit(1);
+  }
+};
+
+seedData();

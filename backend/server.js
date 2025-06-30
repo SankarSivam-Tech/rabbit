e275b8779efb86cfa1232e13abdeae4a6f38e3e0
Veera Sankar <sankarsivam6991@gmail.com>
2025-06-30 18:06:34 +0530

    complete the Cart routes like create,update,delete,get,merge

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/UserRoutes");
 const productRoutes = require("./routes/ProductRoutes");
+const cartRoutes = require("./routes/CartRoutes");
 
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ connectDB();
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
